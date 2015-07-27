9a85627ab502f897f8837a69d352c9cf581a3952
rschumann <sebastian.schkudlara@gmail.com>
2015-07-27 14:45:22 +0200

    Fixes for redis store url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,1 @@
-uri    = URI.parse(ENV["REDISTOGO_URL"])
-$redis = Redis::Namespace.new("gfwc", :redis => Redis.new(url: uri))
+$redis = Redis::Namespace.new("gfwc", :redis => Redis.new(url: ENV["REDISTOGO_URL"]))
