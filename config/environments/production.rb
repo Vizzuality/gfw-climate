c0a60af37478e4aa6095f3acdd4d53e1f38b7b41
rschumann <sebastian.schkudlara@gmail.com>
2015-07-27 13:47:31 +0200

    Heroku fixes for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,10 +14,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.serve_static_assets = true
-
   config.cache_store = :redis_store, ENV['REDISTOGO_URL'], { expires_in: 90.minutes }
-
+  
+  config.serve_static_files = true
+  
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
