13bf9a40b4b1789439407ab0b35437c333b58981
rschumann <sebastian.schkudlara@gmail.com>
2015-08-17 16:26:29 +0200

    Show only enabled countries

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,16 +1,16 @@
 class CountriesController < AccessController
 
   def index
-    # @countries = Country.find_all
+    @countries = Country.find_all
   end
 
   def show
-    # @country   = Country.find_country(params[:id])
+    @country   = Country.find_country(params[:id])
 
-    # @name      = @country['name']
-    # @iso       = @country['params']['iso']
-    # @links     = @country['external_links']             if @country['external_links'].present?
-    # @get_links = JSON.parse(@country['external_links']) if @country['external_links'].present?
+    @name      = @country['name']
+    @iso       = @country['params']['iso']
+    @links     = @country['external_links']             if @country['external_links'].present?
+    @get_links = JSON.parse(@country['external_links']) if @country['external_links'].present?
   end
 
 end
