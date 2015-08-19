c55306d1826415678eb272d64dc9b3eed2a40c4f
Andrés González <andres@simbiotica.es>
2015-08-19 15:54:59 +0200

    continue

--- app/assets/javascripts/countries/views/CountryWindowView.js
+++ app/assets/javascripts/countries/views/CountryWindowView.js
@@ -29,18 +29,7 @@ define([
       this.render();
     },
 
-    _deselectIndicator: function() {
-      this.removeClass('indicator__name--selected');
-    },
-
-    _selectIndicator: function() {
-      this.addClass('indicator__name--selected')
-    },
-
     _toggleIndicator: function(e) {
-      // if($(e.currentTarget).hasClass('indicator__name--selected') {
-
-      // })
       $(e.currentTarget).find('span').toggleClass('indicator__name--selected');
     },
 
@@ -56,80 +45,6 @@ define([
     render: function() {
       this.$el.html(this.template);
     }
-
-    // _initBindings: function() {
-    //   this.mobile = (this.$window.width() > 850) ? false : true;
-    //   this.scrollTop = this.$document.scrollTop();
-    //   if(this.mobile) {
-    //     this.$htmlbody.addClass('active');
-    //     this.$htmlbody.animate({ scrollTop: this.scrollTop },0);
-    //   }
-    //   // document keyup
-    //   this.$document.on('keyup', _.bind(function(e) {
-    //     if (e.keyCode === 27) {
-    //       this.hide();
-    //     }
-    //   },this));
-    //   // backdrop
-    //   this.$backdrop.on('click', _.bind(function() {
-    //     this.hide();
-    //   },this));
-    // },
-
-    // _stopBindings: function() {
-    //   if(this.mobile) {
-    //     this.$htmlbody.removeClass('active');
-    //     this.$htmlbody.animate({ scrollTop: this.scrollTop },0);
-    //   }
-    //   this.$document.off('keyup');
-    //   this.$backdrop.off('click');
-    // },
-
-    // _toggle: function() {
-    //   if (this.model.get('hidden') === true) {
-    //     this._stopBindings();
-    //     this.$sourceWindow.removeClass('active iframe');
-    //     this.$backdrop.removeClass('active');
-    //   } else if (this.model.get('hidden') === false) {
-    //     this._initBindings();
-    //     this.$sourceWindow.addClass('active');
-    //     this.$backdrop.addClass('active');
-    //   }
-    // },
-
-    // hide: function(e) {
-    //   e && e.preventDefault();
-    //   this.model.set('hidden', true);
-    //   // return this;
-    // },
-
-    // show: function(e) {
-    //   e && e.preventDefault() && e.stopPropagation();
-    //   this.model.set('hidden', false);
-    //   // this.$contentWrapper.animate({ scrollTop: 0 }, 0);
-    //   // var data_slug = $(e.currentTarget).data('source');
-    //   // var data_iframe = $(e.currentTarget).data('iframe');
-    //   // (data_iframe) ? this.$sourceWindow.addClass('iframe') : this.$sourceWindow.removeClass('iframe');
-    //   // this.$content.html($('#' + data_slug).clone());
-    //   // return this;
-    // },
-
-    // // showByParam: function(data_slug,link){
-    // //   this.model.set('hidden', false);
-    // //   var $clone = $('#' + data_slug).clone();
-    // //   this.$content.html($clone);
-    // //   if (link) {
-    // //     $clone.find('.set-link').attr('href',link);
-    // //   }
-    // //   return this;
-    // // },
-
-    // render: function() {
-    //   this.$content = this.$sourceWindow.find('.content');
-    //   this.$contentWrapper = this.$sourceWindow.find('.content-wrapper');
-    //   this.$close = this.$sourceWindow.find('.close');
-    //   return this.$sourceWindow;
-    // },
 
   });
   return CountryWindowView;
