fb5f5798d0ac7d9ca9b046dd8f858a5ef0761944
Adrián Pérez <adrian@simbiotica.es>
2015-10-02 11:37:21 +0200

    linking pantropical section with routing

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -12,6 +12,12 @@ class CountriesController < ApplicationController
     @jurisdictions = @country['subnat_bounds']
   end
 
+  def pantropical
+    @title = 'Pantropical visualization'
+    @desc = 'Compare tree cover change across countries and climate domains and view global rankings.'
+    @keywords = 'GFW, list, forest data, visualization, data, national, country, analysis, statistic, tree cover loss, tree cover gain, climate domain, boreal, tropical, subtropical, temperate, deforestation, deforesters, overview, global'
+  end
+
   private
     def filter_params
       params.permit(:id, :thresh)
