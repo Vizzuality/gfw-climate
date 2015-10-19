d7a4cc2c746ddd71206c45dcb70dd18a589f7d3d
Andrés González <andres.gonzalez@vizzuality.com>
2015-10-19 15:20:51 +0200

    About the 1

--- app/assets/javascripts/countries/models/IndicatorModel.js
+++ app/assets/javascripts/countries/models/IndicatorModel.js
@@ -7,6 +7,8 @@ define([
 
     url: '/api/indicators/',
 
+    // We use "1" because API hasn't got more data
+    // Normally, you would use this.id
     initialize: function() {
       this.url += '1/' + this.get('country');
     },
