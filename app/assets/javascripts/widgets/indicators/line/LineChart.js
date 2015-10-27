c4aec46fc1fdd2c42565944edc34d1f9fd8800a7
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-10-27 11:53:46 +0100

    Prevent a graph render if there is no el

--- app/assets/javascripts/widgets/indicators/line/LineChart.js
+++ app/assets/javascripts/widgets/indicators/line/LineChart.js
@@ -26,9 +26,11 @@ var LineChart = function(options) {
   this.parentHeight = $(this.options.el).outerHeight();
   this.width = this.parentWidth - this.sizing.left - this.sizing.right,
   this.height = this.parentHeight - this.sizing.top - this.sizing.bottom;
-  this._createEl();
-  this._createDefs();
-  this._createScales();
+  if (!!this.parentWidth && !!this.parentHeight) {
+    this._createEl();
+    this._createDefs();
+    this._createScales();
+  }
 
   $(window).resize(_.debounce(this.resize.bind(this), 100));
 };
