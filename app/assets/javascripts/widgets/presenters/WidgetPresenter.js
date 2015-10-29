02dff9701518b2226a3c8444f7a22e7c8d3b81a2
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-10-29 11:21:04 +0100

    Reflect all changes to the widget-side

--- app/assets/javascripts/widgets/presenters/WidgetPresenter.js
+++ app/assets/javascripts/widgets/presenters/WidgetPresenter.js
@@ -16,13 +16,20 @@ define([
       this.status = new (Backbone.Model.extend({
         defaults: setup.status
       }));
-
       this._setListeners();
     },
 
     _setListeners: function() {
       this.status.on('change:tabs', this.setTab, this);
     },
+
+    _subscriptions: [{
+      'Compare/reflection': function(id,slug_compare,status) {
+        if (this.model.get('id') == id && this.model.get('slug') == slug_compare) {
+          this.status.set(status);
+        }
+      }
+    }],
 
     changeTab: function(position) {
       var tabs = _.clone(this.status.get('tabs'));
@@ -41,7 +48,10 @@ define([
 
     publish: function() {
       mps.publish('Options/updated', [this.model.get('id'),this.model.get('slug'),this.status.toJSON()]);
-    }
+      if (!!this.model.get('slug_compare')) {
+        mps.publish('Compare/reflection', [this.model.get('id'),this.model.get('slug_compare'),this.status.toJSON()]);
+      }
+    },
 
   });
 
