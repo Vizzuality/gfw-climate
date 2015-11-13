234f51f12aa0dea81e55dcb50976070947baa4ab
Adrián Pérez <adrian@simbiotica.es>
2015-11-13 12:23:43 +0100

    endpoints for carbon stocks and biomass

--- app/assets/javascripts/map/views/layers/BiomassLossLayer.js
+++ app/assets/javascripts/map/views/layers/BiomassLossLayer.js
@@ -18,7 +18,7 @@ define([
     options: {
       threshold: 30,
       dataMaxZoom: 12,
-      urlTemplate: 'http://storage.googleapis.com/thau_wri_loss_for_vizz/full50{z}{/x}{/y}.png'
+      urlTemplate: 'http://storage.googleapis.com/thau_wri_carbon_for_vizz/full50{z}{/x}{/y}.png'
     },
 
     init: function(layer, options, map) {
@@ -38,67 +38,67 @@ define([
      * Filters the canvas imgdata.
      * @override
      */
-      filterCanvasImgdata: function(imgdata, w, h, z) {
-       var components = 4;
-       var exp = z < 11 ? 0.3 + ((z - 3) / 20) : 1;
-       if (! !!this.currentDate[0]._d) {
-         this.currentDate[0] = moment(this.currentDate[0]);
-         this.currentDate[1] = moment(this.currentDate[1]);
-       }
-       var yearStart = this.currentDate[0].year();
-       var yearEnd = this.currentDate[1].year();
+    filterCanvasImgdata: function(imgdata, w, h, z) {
+      var components = 4;
+      var exp = z < 11 ? 0.3 + ((z - 3) / 20) : 1;
+      if (! !!this.currentDate[0]._d) {
+       this.currentDate[0] = moment(this.currentDate[0]);
+       this.currentDate[1] = moment(this.currentDate[1]);
+      }
+      var yearStart = this.currentDate[0].year();
+      var yearEnd = this.currentDate[1].year();
 
-       var myscale = d3.scale.pow()
-           .exponent(exp)
-           .domain([0,256])
-           .range([0,256]);
+      var myscale = d3.scale.pow()
+         .exponent(exp)
+         .domain([0,256])
+         .range([0,256]);
 
-       for(var i = 0; i < w; ++i) {
-         for(var j = 0; j < h; ++j) {
-           var pixelPos = (j * w + i) * components;
-           var intensity = imgdata[pixelPos+1];
-           var yearLoss = 2001 + imgdata[pixelPos];
+      for(var i = 0; i < w; ++i) {
+       for(var j = 0; j < h; ++j) {
+         var pixelPos = (j * w + i) * components;
+         var intensity = imgdata[pixelPos+1];
+         var yearLoss = 2001 + imgdata[pixelPos];
 
-           //intensity=z < 13 ? (intensity)=Math.pow (2, (12-z)) : intensity;
+         //intensity=z < 13 ? (intensity)=Math.pow (2, (12-z)) : intensity;
 
-         
+       
 
-           if (yearLoss >= yearStart && yearLoss <= yearEnd) {
-             
-             if (myscale(intensity) <255){
-                 imgdata[pixelPos] = 189;
-                 imgdata[pixelPos + 1] = 0;
-                 imgdata[pixelPos + 2] = 38;
-             }
-              if (myscale(intensity) <180){
-                 imgdata[pixelPos] = 240;
-                 imgdata[pixelPos + 1] = 59;
-                 imgdata[pixelPos + 2] = 32;
-             }
-             if (myscale(intensity) <100){
-                 imgdata[pixelPos] = 253;
-                 imgdata[pixelPos + 1] = 141;
-                 imgdata[pixelPos + 2] = 60;
-             }
-              if (myscale(intensity) <50){
-                 imgdata[pixelPos] = 240;
-                 imgdata[pixelPos + 1] = 59;
-                 imgdata[pixelPos + 2] = 32;
-             }
-              if (myscale(intensity) <20){
-                 imgdata[pixelPos] = 255;
-                 imgdata[pixelPos + 1] = 255;
-                 imgdata[pixelPos + 2] = 178;
-             }
-             
-             imgdata[pixelPos + 3] = z < 13 ? myscale(intensity) : intensity;;
-          } else {
-            imgdata[pixelPos + 3] = 0;
+         if (yearLoss >= yearStart && yearLoss <= yearEnd) {
+           
+           if (myscale(intensity) <255){
+               imgdata[pixelPos] = 189;
+               imgdata[pixelPos + 1] = 0;
+               imgdata[pixelPos + 2] = 38;
            }
-
+            if (myscale(intensity) <180){
+               imgdata[pixelPos] = 240;
+               imgdata[pixelPos + 1] = 59;
+               imgdata[pixelPos + 2] = 32;
+           }
+           if (myscale(intensity) <100){
+               imgdata[pixelPos] = 253;
+               imgdata[pixelPos + 1] = 141;
+               imgdata[pixelPos + 2] = 60;
+           }
+            if (myscale(intensity) <50){
+               imgdata[pixelPos] = 240;
+               imgdata[pixelPos + 1] = 59;
+               imgdata[pixelPos + 2] = 32;
+           }
+            if (myscale(intensity) <20){
+               imgdata[pixelPos] = 255;
+               imgdata[pixelPos + 1] = 255;
+               imgdata[pixelPos + 2] = 178;
+           }
+           
+           imgdata[pixelPos + 3] = z < 13 ? myscale(intensity) : intensity;;
+        } else {
+          imgdata[pixelPos + 3] = 0;
          }
+
        }
-     },
+      }
+    },
 
     /**
      * Used by UMDLoassLayerPresenter to set the dates for the tile.
