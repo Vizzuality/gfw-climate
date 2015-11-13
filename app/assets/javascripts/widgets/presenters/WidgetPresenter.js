6b393744ce415e3f456b0451f9e1d6bbe014353d
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-11-13 13:52:13 +0100

    Number widget needs a template variable. All templates created

--- app/assets/javascripts/widgets/presenters/WidgetPresenter.js
+++ app/assets/javascripts/widgets/presenters/WidgetPresenter.js
@@ -32,6 +32,10 @@ define([
     }],
 
     changeTab: function(position) {
+      // ******
+      // CAREFUL: if you add anything new to the widgets.json
+      //          remember to add it inside CompareGridPresenter (getTabsOptions function) and inside widgetPresenter (changeTab function)
+      // ******
       var tabs = _.clone(this.status.get('tabs'));
       var t = _.findWhere(this.model.get('tabs'), { position: position });
       tabs = {
@@ -42,6 +46,7 @@ define([
         end_date: (t.range) ? t['range'][t['range'].length - 1] : null,
         thresh: (t.thresh) ? t['thresh'] : 0,
         section: (t.sectionswitch) ? t['sectionswitch'][0]['unit'] : null,
+        template: (t.template) ? t['template'] : null,
       }
       this.status.set('tabs',tabs);
     },
