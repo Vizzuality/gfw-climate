a4084acf2f29fe546a83552294b3c52eef00aa29
Adrián Pérez <adrian@simbiotica.es>
2015-12-01 15:43:44 +0100

    force a 32int strong type for each value

--- app/assets/javascripts/map/views/layers/BiomassLossLayer.js
+++ app/assets/javascripts/map/views/layers/BiomassLossLayer.js
@@ -39,63 +39,72 @@ define([
      * @override
      */
     filterCanvasImgdata: function(imgdata, w, h, z) {
-      var components = 4;
-      var exp = z < 11 ? 0.3 + ((z - 3) / 20) : 1;
+      "use asm";
+      // We'll force the use of a 32bit integer wit `value |0`
+      // More info here: http://asmjs.org/spec/latest/
+
+      var components = 4 | 0,
+          w = w |0,
+          j = j |0,
+          z = z |0,
+          exp = z < 11 ? 0.3 + ((z - 3) / 20) : 1 | 0;
+
       if (! !!this.currentDate[0]._d) {
        this.currentDate[0] = moment(this.currentDate[0]);
        this.currentDate[1] = moment(this.currentDate[1]);
       }
-      var yearStart = this.currentDate[0].year();
-      var yearEnd = this.currentDate[1].year();
+      var yearStart = this.currentDate[0].year(),
+          yearEnd = this.currentDate[1].year();
 
       var myscale = d3.scale.pow()
          .exponent(exp)
          .domain([0,256])
          .range([0,256]);
 
-      for(var i = 0; i < w; ++i) {
-       for(var j = 0; j < h; ++j) {
-         var pixelPos = (j * w + i) * components;
-         var intensity = imgdata[pixelPos+1];
-         var yearLoss = 2001 + imgdata[pixelPos];
+      for(var i = 0 |0; i < w; ++i) {
+       for(var j = 0 |0; j < h; ++j) {
+         var pixelPos = ((j * w + i) * components) |0,
+             intensity = imgdata[pixelPos+1] |0,
+             intensity_scaled = myscale(intensity) |0,
+             yearLoss = 2001 + imgdata[pixelPos] |0;
 
          //intensity=z < 13 ? (intensity)=Math.pow (2, (12-z)) : intensity;
 
          if (yearLoss >= yearStart && yearLoss <= yearEnd) {
-          imgdata[pixelPos] = 0;
-          if (myscale(intensity) < 1){
+          imgdata[pixelPos] = 0 | 0;
+          if (intensity_scaled < 1){
                imgdata[pixelPos + 3] = imgdata[pixelPos + 2] = imgdata[pixelPos + 1] = 0;
            }
-          else if (myscale(intensity) < 50){
-               imgdata[pixelPos + 1] = 112;
-               imgdata[pixelPos + 2] = 168;
-               imgdata[pixelPos + 3] = 256;
+          else if (intensity_scaled < 50){
+               imgdata[pixelPos + 1] = 112 | 0;
+               imgdata[pixelPos + 2] = 168 | 0;
+               imgdata[pixelPos + 3] = 256 | 0;
           }
-          else if (myscale(intensity) < 100){
-               imgdata[pixelPos]     = 76;
-               imgdata[pixelPos + 1] = 83; 
-               imgdata[pixelPos + 2] = 122;
-               imgdata[pixelPos + 3] = 255;
+          else if (intensity_scaled < 100){
+               imgdata[pixelPos]     = 76 | 0;
+               imgdata[pixelPos + 1] = 83 | 0; 
+               imgdata[pixelPos + 2] = 122 | 0;
+               imgdata[pixelPos + 3] = 255 | 0;
            }
-          else if (myscale(intensity) < 150){
-               imgdata[pixelPos]     = 210;
-               imgdata[pixelPos + 1] = 31; 
-               imgdata[pixelPos + 2] = 38;
-               imgdata[pixelPos + 3] = 255;
+          else if (intensity_scaled < 150){
+               imgdata[pixelPos]     = 210 | 0;
+               imgdata[pixelPos + 1] = 31 | 0; 
+               imgdata[pixelPos + 2] = 38 | 0;
+               imgdata[pixelPos + 3] = 255 | 0;
           }
-          else if (myscale(intensity) < 200){
-               imgdata[pixelPos]     = 241;
-               imgdata[pixelPos + 1] = 152;
-               imgdata[pixelPos + 2] = 19;
-               imgdata[pixelPos + 3] = 255;
+          else if (intensity_scaled < 200){
+               imgdata[pixelPos]     = 241 | 0;
+               imgdata[pixelPos + 1] = 152 | 0;
+               imgdata[pixelPos + 2] = 19 | 0;
+               imgdata[pixelPos + 3] = 255 | 0;
           }
-          else if (myscale(intensity) < 256){
-               imgdata[pixelPos + 3] = imgdata[pixelPos] = 255;
-               imgdata[pixelPos + 1] = 208;
-               imgdata[pixelPos + 2] = 11;
+          else if (intensity_scaled < 256){
+               imgdata[pixelPos + 3] = imgdata[pixelPos] = 255 | 0;
+               imgdata[pixelPos + 1] = 208 | 0;
+               imgdata[pixelPos + 2] = 11 | 0;
           }
         } else {
-          imgdata[pixelPos + 3] = 0;
+          imgdata[pixelPos + 3] = 0 | 0;
         }
        }
       }
