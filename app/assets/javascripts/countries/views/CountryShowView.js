2f8c5db88716c21d455f2d2cddcabbce6cb4dca2
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2015-12-03 17:25:42 +0100

    Modal added to country profile. Behaviour missing

--- app/assets/javascripts/countries/views/CountryShowView.js
+++ app/assets/javascripts/countries/views/CountryShowView.js
@@ -5,12 +5,14 @@ define([
   'countries/views/show/TabsView',
   'countries/views/show/CountryWidgetsModalView',
   'countries/views/show/WidgetGridView',
+  'countries/views/show/CountryGridButtonBoxView',
   // Common views
   'views/SourceModalView',
   'views/ToolbarView',
 
 ], function(Backbone, FixedHeaderView,
-  CountryShowHeaderView, TabsView, CountryWidgetsModalView, WidgetGridView, SourceModalView, ToolbarView) {
+  CountryShowHeaderView, TabsView, CountryWidgetsModalView, WidgetGridView, CountryGridButtonBoxView,
+  SourceModalView, ToolbarView) {
   'use strict';
 
   var CountryShowView = Backbone.View.extend({
@@ -21,6 +23,7 @@ define([
       new FixedHeaderView();
       new TabsView();
       new WidgetGridView();
+      new CountryGridButtonBoxView();
       // Common views
       new SourceModalView();
       new ToolbarView();
