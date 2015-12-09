359979cca5c687ff253a2982a62f3e08acda3161
Clara Linos <clara.linos@gmail.com>
2015-12-09 12:40:46 +0100

    correct literal

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -3,7 +3,7 @@ class CountriesController < ApplicationController
   before_action :set_country, only: :show
 
   def index
-    @title = 'Countries profile'
+    @title = 'Country profiles'
     @countries = Country.find_all
   end
 
