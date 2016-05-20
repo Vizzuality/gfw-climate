7ff557a189a16f90ee515e3efc7f6da49f61f584
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-05-20 11:44:15 +0200

    UI events completed

--- app/assets/javascripts/widgets/views/ShareWidgetView.js
+++ app/assets/javascripts/widgets/views/ShareWidgetView.js
@@ -33,12 +33,11 @@ define([
     events: {
       'click .js-close-share' : 'hide',
       'click .js-type-share' : 'changeType',
-      // 'click #share_field' : '_selectTarget',
-      // 'click .change-type' : '_setTypeFromEvent',
+      'click .js-copy-share' : 'copyUrl',
+      'focus .js-input-share' : 'selectUrl',
+      'click .js-social-share' : 'openPopup',     
       // 'click #preview' : '_showPreview',
-      // 'click .share-sozial a' : '_shareToSocial',
-      // 'click .overlay' : 'hide',
-      // 'click .copy_url' : '_copyToClipboard'
+
     },
 
     initialize: function(parent) {
@@ -59,6 +58,12 @@ define([
     cache: function() {
       this.$body = $('body');
       this.$input = this.$el.find('#share-field');
+
+      // Social links
+      this.$twitterLink = this.$('.twitter');
+      this.$facebookLink = this.$('.facebook');
+      this.$googleplusLink = this.$('.google_plus');
+
     },
 
     listeners: function() {
@@ -133,21 +138,11 @@ define([
       this.$input.val(this.model.get('url'));
     },
 
-    // UI EVENTS
-    changeType: function(e) {
-      e && e.preventDefault()
-      this.model.set('type', $(e.currentTarget).data('type'));
-    },
-
-
     // LINK
     setUrlLink: function() {
       this.getBitlyLink(window.location.href, function(url) {
         this.model.set('url', url);
-        // this.$input.val(url);
-        // this.$twitterLink.attr('href', 'https://twitter.com/share?url=' + url);
-        // this.$facebookLink.attr('href', 'https://www.facebook.com/sharer.php?u=' + url);
-        // this.$google_plusLink.attr('href', 'https://plus.google.com/share?url=' + url);
+        this.setSocialLinks();
       }.bind(this));
       ga('send', 'event', 'Map', 'Share', 'Share Link clicked');
     },
@@ -180,75 +175,56 @@ define([
     },
 
     getEmbedLink: function() {
-      console.log(this.model.get('slugshare'));
       var width = 600;
       var height = 600;
-      var url = '';
+      var url = window.location.protocol + '//' + window.location.host + '/embed/countries/'+ this.model.get('slugshare') +'/'+ this.model.get('widget');
       return '<iframe width="' +width+ '" height="' +height+ '" frameborder="0" src="' + url + '"></iframe>';
-    }
+    },
 
-    // _renderEmbed: function() {
-    //   this.$input.val(this._generateEmbedSrc());
+    // SOCIAL
+    setSocialLinks: function() {
+      this.$twitterLink.attr('href', 'https://twitter.com/share?url=' + this.model.get('url'));
+      this.$facebookLink.attr('href', 'https://www.facebook.com/sharer.php?u=' + this.model.get('url'));
+      this.$googleplusLink.attr('href', 'https://plus.google.com/share?url=' + this.model.get('url'));
+    },
 
-    //   this.$shareinfo.html('Click and paste HTML to embed in website.');
-    //   // Only show preview on desktop, mobile preview is quite fiddly
-    //   // for the user
-    //   if (!this._isMobile()) {
-    //     this.$shareinfo.append('<button id="preview" class="btn gray little uppercase">Preview</button></p>');
-    //   }
+    
+    // UI EVENTS
+    changeType: function(e) {
+      e && e.preventDefault()
+      this.model.set('type', $(e.currentTarget).data('type'));
+    },
 
-    //   ga('send', 'event', 'Map', 'Share', 'Share Embed clicked');
-    // },
+    copyUrl: function(e) {
+      this.$input.select();
+      try {
+        var successful = document.execCommand('copy');
+        $(e.currentTarget).html('copied')
+      } catch(err) {
+        mps.publish('Notification/open', ['not-clipboard-support']);
+      }
+    },
 
-    // _generateEmbedSrc: function() {
-    //   var dim_x = this.model.get('embedWidth') || 600, dim_y = this.model.get('embedHeight') || 600;
-    //   return '<iframe width="' +dim_x+ '" height="' +dim_y+ '" frameborder="0" src="' + this.model.get('embedUrl') + '"></iframe>';
-    // },
+    selectUrl: function(e) {
+      this.$input.select();
+    },
 
-    // _setUrlsFromEvent: function(event) {
-    //   var hideEmbed = $(event.currentTarget).data('hideembed');
-    //   this.model.set('hideEmbed', !!hideEmbed);
-    //   this.render();
+    openPopup: function(e) {
+      e && e.preventDefault();
 
-    //   var url = $(event.currentTarget).data('share-url') || window.location.href;
-    //   this.model.set('url', url);
+      var width  = 575,
+          height = 400,
+          left   = ($(window).width()  - width)  / 2,
+          top    = ($(window).height() - height) / 2,
+          url    = $(e.currentTarget).attr('href'),
+          opts   = 'status=1' +
+                   ',width='  + width  +
+                   ',height=' + height +
+                   ',top='    + top    +
+                   ',left='   + left;
 
-    //   var embedUrl = $(event.currentTarget).data('share-embed-url') || window.location.origin + '/embed' + window.location.pathname + window.location.search;
-    //   this.model.set('embedUrl', embedUrl);
-    //   this.model.set('embedWidth', $(event.currentTarget).data('share-embed-width'));
-    //   this.model.set('embedHeight', $(event.currentTarget).data('share-embed-height'));
-
-
-
-    // },
-
-    // _setTypeFromEvent: function(event) {
-    //   this.$copy.html('copy')
-    //   var target_type = $(event.currentTarget).data('type');
-    //   var type = target_type || this.model.get('type') || 'link';
-    //   this.model.set('type', type);
-    // },
-
-    // _toggleTypeButtons: function() {
-    //   this.$changeType.toggleClass('blue').toggleClass('gray');
-    //   this._renderInput();
-    // },
-
-    // _selectTarget: function(e) {
-    //   $(e.currentTarget).select();
-    // },
-
-    // _copyToClipboard: function(e) {
-    //   var url = document.querySelector('#share_field');
-    //   url.select();
-
-    //   try {
-    //     var successful = document.execCommand('copy');
-    //     $(e.currentTarget).html('copied')
-    //   } catch(err) {
-    //     mps.publish('Notification/open', ['not-clipboard-support']);
-    //   }
-    // },
+      window.open(url, 'Share this map view', opts);
+    },
 
     // _showPreview: function() {
     //   this.iframeView = ne({
@@ -260,22 +236,6 @@ define([
     //   $('body').append(this.iframeView.render().$el);
     // },
 
-    // _shareToSocial: function(e) {
-    //   e && e.preventDefault();
-
-    //   var width  = 575,
-    //       height = 400,
-    //       left   = ($(window).width()  - width)  / 2,
-    //       top    = ($(window).height() - height) / 2,
-    //       url    = $(e.currentTarget).attr('href'),
-    //       opts   = 'status=1' +
-    //                ',width='  + width  +
-    //                ',height=' + height +
-    //                ',top='    + top    +
-    //                ',left='   + left;
-
-    //   window.open(url, 'Share this map view', opts);
-    // },
 
     // _isMobile: function() {
     //   return ($(window).width() > 850) ? false : true;
