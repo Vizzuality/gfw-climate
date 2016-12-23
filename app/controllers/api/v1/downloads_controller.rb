a131f6101fe8b96b6974b3a055cd693cf309e237
Simao Belchior <simaobelchior@gmail.com>
2016-12-23 17:59:03 +0000

    WIP: Indicators download, starting to setup backend

---
+++ app/controllers/api/v1/downloads_controller.rb
@@ -0,0 +1,14 @@
+module Api::V!
+  class DownloadsController < BaseControllerV!
+    def index
+      zip_file = Download.new(download_params).as_zip
+    end
+
+    private
+
+    def download_params
+      params.permit(:iso, :id_1, :area, :start_year, :end_year,
+                    units: [], indicator_ids: [], thresholds: [])
+    end
+  end
+end
---
+++ app/models/download.rb
@@ -0,0 +1,23 @@
+require 'httparty'
+
+class Download
+  include HTTParty
+
+  attr_accessor :iso, :id_1, :area, :start_year, :end_year,
+    :units, :indicator_ids, :thresholds
+
+  def initialize options
+    @iso = options[:iso]
+    @id_1 = options[:id_1]
+    @area = options[:area]
+    @start_year = options[:start_year]
+    @end_year = options[:end_year]
+    @units = options[:units]
+    @indicator_ids = options[:indicator_ids]
+    @thresholds = options[:thresholds]
+  end
+
+  def as_zip
+    []
+  end
+end
