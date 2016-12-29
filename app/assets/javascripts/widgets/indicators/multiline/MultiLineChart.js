bb7fbbe4acf345dd6a292f89404c983da2f34ccb
j8seangel <jseangel@gmail.com>
2016-12-29 11:02:00 +0100

    Fix float axis

--- app/assets/javascripts/widgets/indicators/multiline/MultiLineChart.js
+++ app/assets/javascripts/widgets/indicators/multiline/MultiLineChart.js
@@ -93,7 +93,7 @@ LineChart.prototype._createDefs = function() {
 LineChart.prototype._drawAxes = function() {
   var self = this;
   var tickSizeX = ($(window).width() > 1025) ? 1 : 2;
-  var tickFormatY = (this.unit != 'percentage') ? "s" : ".2f";
+  var tickFormatY = (this.unit != 'percentage') ? ".2s" : ".2f";
   this.xAxis = d3.svg.axis()
                  .scale(self.x)
                  .ticks(d3.time.year, tickSizeX)
