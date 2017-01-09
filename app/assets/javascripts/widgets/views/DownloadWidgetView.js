d92856164d3dda5b21603859dafc4a5de9afb241
j8seangel <jseangel@gmail.com>
2017-01-09 19:20:03 +0100

    New rounded select style

--- app/assets/javascripts/widgets/views/DownloadWidgetView.js
+++ app/assets/javascripts/widgets/views/DownloadWidgetView.js
@@ -2,10 +2,11 @@ define([
   'jquery',
   'backbone',
   'handlebars',
+  'chosen',
   'views/ModalView',
   'widgets/presenters/DownloadWidgetPresenter',
   'text!widgets/templates/widget-download.handlebars',
-], function($, Backbone, Handlebars, ModalView, DownloadWidgetPresenter, tpl) {
+], function($, Backbone, Handlebars, chosen, ModalView, DownloadWidgetPresenter, tpl) {
 
   var DownloadModalView = ModalView.extend({
 
@@ -20,10 +21,32 @@ define([
       this.constructor.__super__.initialize.apply(this);
       this.presenter = new DownloadWidgetPresenter(this);
       this.options = _.extend(this.defaults, options);
+      this.setMobile();
       this.setListeners();
       this._initVars();
       this.$body = $('body');
       this.$body.append(this.el);
+    },
+
+    setMobile: function() {
+       enquire.register("screen and (max-width:"+window.gfw.config.GFW_MOBILE+"px)", {
+        match: _.bind(function(){
+          this.mobile = true;
+        },this)
+      });
+
+      enquire.register("screen and (min-width:"+window.gfw.config.GFW_MOBILE+"px)", {
+        match: _.bind(function(){
+          this.mobile = false;
+        },this)
+      });
+    },
+
+    initChosen: function() {
+      this.$selects = this.$el.find('.chosen-select');
+      this.$selects.chosen({
+        disable_search: true
+      });
     },
 
     setListeners: function(e) {
@@ -37,7 +60,7 @@ define([
     },
 
     handleSelectChange: function(e) {
-      var status = {}
+      var status = {};
       var target = e.currentTarget;
       status[target.dataset.attribute] = target.options[target.selectedIndex].value;
       this.presenter.updateStatus(status);
@@ -50,6 +73,9 @@ define([
 
     render: function(data) {
       this.$el.html(this.template(data));
+      if (!this.mobile) {
+        this.initChosen();
+      }
       return this;
     },
 
