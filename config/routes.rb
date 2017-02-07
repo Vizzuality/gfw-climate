f3e237ea5df83f36bc419de131dd5f84af49d174
j8seangel <jseangel@gmail.com>
2017-02-07 15:14:18 +0100

    API endpoint to email subscription

--- config/routes.rb
+++ config/routes.rb
@@ -75,6 +75,8 @@ Rails.application.routes.draw do
       # Compare countries API routes
       # GET 'compare-countries/bra+1+0/aus+1+0/aut+0+3/etc...'
       get 'compare-countries(/*path)', to: 'compare_countries#index', as: :compare_countries
+
+      post 'sign-up', to: 'sign_up_updates#create'
     end
 
   end
