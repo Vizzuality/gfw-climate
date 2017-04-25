dd0b8290e98d479454ad644ba0b153a22b20dc49
j8seangel <jseangel@gmail.com>
2017-04-25 16:10:35 +0200

    Geo list view

--- app/assets/javascripts/views/shared/GeoListView.js
+++ app/assets/javascripts/views/shared/GeoListView.js
@@ -1,40 +1,39 @@
-define([
-  'backbone',
-  'handlebars',
-  'helpers/CountryHelper',
-  'text!templates/shared/geo-list.handlebars',
-], function(Backbone, Handlebars, CountryHelper, tpl) {
+define(
+  [
+    'backbone',
+    'handlebars',
+    'helpers/CountryHelper',
+    'text!templates/shared/geo-list.handlebars'
+  ],
+  function(Backbone, Handlebars, CountryHelper, tpl) {
+    'use strict';
+    var GeoListView = Backbone.View.extend({
+      template: Handlebars.compile(tpl),
 
-  'use strict';
+      initialize: function(settings) {
+        this.params = _.extend({}, this.defaults, settings);
+        this.render(this.params);
+        this.renderGeo(this.params.data);
+      },
 
-  var GeoListView = Backbone.View.extend({
+      render: function(data) {
+        this.$el.html(this.template(data));
+      },
 
-    template: Handlebars.compile(tpl),
-
-    initialize: function(settings) {
-      this.params = _.extend({}, this.defaults, settings);
-      this.render(this.params);
-      this.renderGeo(this.params.data);
-    },
-
-    render: function(data) {
-      this.$el.html(this.template(data));
-    },
-
-    renderGeo: function(data) {
-      if (data && data.length) {
-        for (var i = 0, dLength = data.length; i < dLength; i++) {
-          if (data[i].topojson) {
-            CountryHelper.draw(data[i].topojson, 0, data[i].iso + '-geometry', {
-              width: 150,
-              height: 150
-            });
+      renderGeo: function(data) {
+        if (data && data.length) {
+          for (var i = 0, dLength = data.length; i < dLength; i++) {
+            if (data[i].topojson) {
+              var el = '#' + data[i].iso + '-geometry';
+              CountryHelper.draw(data[i].topojson, el, {
+                width: 150,
+                height: 150
+              });
+            }
           }
         }
       }
-    }
-  });
-
-  return GeoListView;
-
-});
+    });
+    return GeoListView;
+  }
+);
