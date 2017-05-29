51106ca22239e192cef184c8fb2696ca818a9e12
Benjamin A. Laken <benlaken@gmail.com>
2017-05-29 10:50:14 +0200

    removed tropics focus, as new data layers are global

--- app/assets/javascripts/map/views/MapView.js
+++ app/assets/javascripts/map/views/MapView.js
@@ -462,7 +462,9 @@ define([
       for (var i = 1999; i < 2013; i++) {
         this.map.mapTypes.set('landsat{0}'.format(i), landsatMaptype([i]));
       }
-      this.map.data.addGeoJson(JSON.parse(tropicsOverlay));
+      // The below line greys out areas outside of the tropics, using a
+      // geojson in map/geojson_overlays/tropics.json. Deactivated 29th May 2017
+      //this.map.data.addGeoJson(JSON.parse(tropicsOverlay));
     },
 
     /**
