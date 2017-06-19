a45e6fa8134fc864a0e03c5a17fcf6d34d3e246d
j8seangel <jseangel@gmail.com>
2017-06-19 17:17:57 +0200

    Data download page placeholder

---
+++ app/assets/javascripts/data-download/views/DataDownloadIndexView.js
@@ -0,0 +1,10 @@
+define(['backbone'], function(Backbone) {
+  'use strict';
+  var DataDownloadIndexView = Backbone.View.extend({
+    initialize: function() {
+      console.log('view initialized');
+    }
+  });
+
+  return DataDownloadIndexView;
+});
