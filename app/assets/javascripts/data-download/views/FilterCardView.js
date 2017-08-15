a847a08695f975ab8ff37566c2abc067568524f2
j8seangel <jseangel@gmail.com>
2017-08-15 17:47:53 +0200

    Filter card view

---
+++ app/assets/javascripts/data-download/views/FilterCardView.js
@@ -0,0 +1,11 @@
+define(['backbone'], function(Backbone) {
+  'use strict';
+  var FilterCardView = Backbone.View.extend({
+
+    initialize: function(options) {
+      console.log(options);
+    },
+  });
+
+  return FilterCardView;
+});
