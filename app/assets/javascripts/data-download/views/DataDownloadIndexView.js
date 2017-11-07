210cdfe324c82283096abd8b995bb02f45419945
Simao Belchior <simaobelchior@gmail.com>
2017-11-07 11:49:03 +0100

    Adjusts the id name for areas of interest box to match backend's expectations

--- app/assets/javascripts/data-download/views/DataDownloadIndexView.js
+++ app/assets/javascripts/data-download/views/DataDownloadIndexView.js
@@ -35,7 +35,7 @@ define(
           placeholder: 'Select a country'
         },
         {
-          id: 'areas_of_interest',
+          id: 'area_ids',
           name: 'Areas of interest',
           placeholder: 'Select a country'
         },
@@ -379,6 +379,7 @@ define(
 
       onCountryChange: function(selection) {
         var jurisIndex = this.filterIndex.jurisdiction_ids;
+        var aoiIndex = this.filterIndex.area_ids;
         var jurisView = this.filterViews[jurisIndex];
         if (jurisView) {
           if (selection && selection.length === 1) {
