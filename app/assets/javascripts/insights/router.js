b37a38a1228187e6801a2a1b56c6daae9f5b2923
j8seangel <jseangel@gmail.com>
2018-02-14 18:11:16 +0100

    Fix old embed insight

--- app/assets/javascripts/insights/router.js
+++ app/assets/javascripts/insights/router.js
@@ -1,134 +1,141 @@
-define([
-  'backbone',
-  'mps',
-  'urijs/URI',
-  'utils',
-  'countries/services/PlaceService',
-  'insights/views/glad-alerts/InsightsGladAlertsView',
-  'insights/views/emissions-calculator/EmissionCalculatorIndexView',
-  'insights/views/emissions-calculator/EmissionCalculatorShowView'
-], function(
-  Backbone,
-  mps,
-  URI,
-  utils,
-  PlaceService,
-  InsightsGladAlertsView,
-  EmissionCalculatorIndexView,
-  EmissionCalculatorShowView
-) {
+define(
+  [
+    'backbone',
+    'underscore',
+    'mps',
+    'urijs/URI',
+    'utils',
+    'countries/services/PlaceService',
+    'insights/views/glad-alerts/InsightsGladAlertsView',
+    'insights/views/emissions-calculator/EmissionCalculatorIndexView',
+    'insights/views/emissions-calculator/EmissionCalculatorShowView'
+  ],
+  function(
+    Backbone,
+    _,
+    mps,
+    URI,
+    utils,
+    PlaceService,
+    InsightsGladAlertsView,
+    EmissionCalculatorIndexView,
+    EmissionCalculatorShowView
+  ) {
+    'use strict';
 
-  'use strict';
+    var Router = Backbone.Router.extend({
+      params: new (Backbone.Model.extend())(),
 
-  var Router = Backbone.Router.extend({
+      routes: {
+        'insights/glad-alerts(/:iso)': '_initGladAlerts',
+        'insights/emissions-calculator': '_initEmissionsCalculatorIndex',
+        'insights/emissions-calculator/:id': '_initEmissionsCalculatorShow'
+      },
 
-    params: new (Backbone.Model.extend()),
+      initialize: function() {
+        this.placeService = new PlaceService(this);
 
-    routes: {
-      'insights/glad-alerts(/:iso)': '_initGladAlerts',
-      'insights/emissions-calculator': '_initEmissionsCalculatorIndex',
-      'insights/emissions-calculator/:id': '_initEmissionsCalculatorShow',
-    },
+        this.setSubscriptions();
+        this.setEvents();
+      },
 
-    initialize: function() {
-      this.placeService = new PlaceService(this);
+      setSubscriptions: function() {
+        mps.subscribe('Router/change', this.setParams.bind(this));
+        mps.subscribe('Router/goInsight', this.updateInsight.bind(this));
+      },
 
-      this.setSubscriptions();
-      this.setEvents();
-    },
+      setEvents: function() {
+        this.params.on('change', this.updateUrl, this);
+      },
 
-    setSubscriptions: function() {
-      mps.subscribe('Router/change', this.setParams.bind(this));
-      mps.subscribe('Router/goInsight', this.updateInsight.bind(this));
-    },
+      /**
+       * Setting new params and update it
+       * @param {Object} params
+       */
+      setParams: function(params) {
+        this.params.clear({ silent: true }).set(params);
+      },
 
-    setEvents: function() {
-      this.params.on('change', this.updateUrl, this);
-    },
+      /**
+       * Namespace to get current params
+       */
+      getParams: function() {
+        return this.params.attributes;
+      },
 
-    /**
-     * Setting new params and update it
-     * @param {Object} params
-     */
-    setParams: function(params) {
-      this.params.clear({ silent: true }).set(params);
-    },
-
-    /**
-     * Namespace to get current params
-     */
-    getParams: function() {
-      return this.params.attributes;
-    },
-
-    /**
-     * Change URL with current params
-     */
-    updateUrl: function() {
-      var uri = new URI();
-      var params = _.omit(this.getParams(), 'vars', 'defaults','params');
-      uri.query(this._serializeParams(params));
-      // {replace: true} update the URL without creating an entry in the browser's history and allow us to go back
-      this.navigate(uri.path().slice(1) + uri.search(), { trigger: true, replace: true });
-    },
-
-    /**
-     * Update the country insight
-     */
-    updateInsight: function(iso) {
-      this.navigate('/insights/' + this.insight + '/' + iso, { trigger: false });
-    },
-
-    /**
-     * Transform URL string params to object
-     * @param  {String} paramsQuery
-     * @return {Object}
-     * @example https://medialize.github.io/URI.js/docs.html
-     */
-    _unserializeParams: function(paramsQuery) {
-      var params = {};
-      if (typeof paramsQuery === 'string' && paramsQuery.length) {
+      /**
+       * Change URL with current params
+       */
+      updateUrl: function() {
         var uri = new URI();
-        uri.query(paramsQuery);
-        params = uri.search(true);
+        var params = _.omit(this.getParams(), 'vars', 'defaults', 'params');
+        uri.query(this._serializeParams(params));
+        // {replace: true} update the URL without creating an entry in the browser's history and allow us to go back
+        this.navigate(uri.path().slice(1) + uri.search(), {
+          trigger: true,
+          replace: true
+        });
+      },
+
+      /**
+       * Update the country insight
+       */
+      updateInsight: function(iso) {
+        this.navigate('/insights/' + this.insight + '/' + iso, {
+          trigger: false
+        });
+      },
+
+      /**
+       * Transform URL string params to object
+       * @param  {String} paramsQuery
+       * @return {Object}
+       * @example https://medialize.github.io/URI.js/docs.html
+       */
+      _unserializeParams: function(paramsQuery) {
+        var params = {};
+        if (typeof paramsQuery === 'string' && paramsQuery.length) {
+          var uri = new URI();
+          uri.query(paramsQuery);
+          params = uri.search(true);
+        }
+        return params;
+      },
+
+      /**
+       * Transform object params to URL string
+       * @param  {Object} params
+       * @return {String}
+       * @example https://medialize.github.io/URI.js/docs.html
+       */
+      _serializeParams: function(params) {
+        var uri = new URI();
+        uri.search(params);
+        return uri.search();
+      },
+
+      _initGladAlerts: function(iso) {
+        this.iso = iso;
+        this.insight = 'glad-alerts';
+
+        new InsightsGladAlertsView({
+          country: this.iso
+        });
+      },
+
+      _initEmissionsCalculatorIndex: function() {
+        this.insight = 'emissions-calculator';
+        new EmissionCalculatorIndexView();
+      },
+
+      _initEmissionsCalculatorShow: function(id) {
+        this.insight = 'emissions-calculator';
+        new EmissionCalculatorShowView({
+          id: id
+        });
       }
-      return params;
-    },
+    });
 
-    /**
-     * Transform object params to URL string
-     * @param  {Object} params
-     * @return {String}
-     * @example https://medialize.github.io/URI.js/docs.html
-     */
-    _serializeParams: function(params) {
-      var uri = new URI();
-      uri.search(params);
-      return uri.search();
-    },
-
-    _initGladAlerts: function(iso) {
-      this.iso = iso;
-      this.insight = 'glad-alerts';
-
-      new InsightsGladAlertsView({
-        country: this.iso
-      });
-    },
-
-    _initEmissionsCalculatorIndex: function() {
-      this.insight = 'emissions-calculator';
-      new EmissionCalculatorIndexView();
-    },
-
-    _initEmissionsCalculatorShow: function(id) {
-      this.insight = 'emissions-calculator';
-      new EmissionCalculatorShowView({
-        id: id
-      });
-    }
-  });
-
-  return Router;
-
-});
+    return Router;
+  }
+);
