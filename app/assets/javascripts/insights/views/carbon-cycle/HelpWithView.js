438dba0b8c6307d7eb79570ac0384a3e4a4419c8
j8seangel <jseangel@gmail.com>
2018-03-26 22:07:42 +0200

    Basic styles and logic change

--- app/assets/javascripts/insights/views/carbon-cycle/HelpWithView.js
+++ app/assets/javascripts/insights/views/carbon-cycle/HelpWithView.js
@@ -14,7 +14,7 @@ define(
       slides: [
         {
           id: 1,
-          title: 'Help restore forest',
+          title: 'Help restore forest 1',
           content:
             'Support local and international campaigns to restore forest landscapes. There are many organizations that actively reverse the effects of deforestation like Trillion Trees camapaign, TNC, arbor day foundation, Plant Your Future',
           img: '/assets/insights/carbon-cycle/last-block-img1@2x.jpg',
@@ -22,7 +22,7 @@ define(
         },
         {
           id: 2,
-          title: 'Help restore forest',
+          title: 'Help restore forest 2',
           content:
             'Support local and international campaigns to restore forest landscapes. There are many organizations that actively reverse the effects of deforestation like Trillion Trees camapaign, TNC, arbor day foundation, Plant Your Future',
           img: '/assets/insights/carbon-cycle/last-block-img2@2x.jpg',
@@ -30,15 +30,20 @@ define(
         },
         {
           id: 3,
-          title: 'Help restore forest',
+          title: 'Help restore forest 3',
           content:
             'Support local and international campaigns to restore forest landscapes. There are many organizations that actively reverse the effects of deforestation like Trillion Trees camapaign, TNC, arbor day foundation, Plant Your Future',
           img: '/assets/insights/carbon-cycle/last-block-img3@2x.jpg',
           bg: '/assets/insights/carbon-cycle/last-block-bg3@2x.jpg'
         }
       ],
-      activeIndex: 1,
+      activeIndex: 0,
+
       template: Handlebars.compile(tpl),
+
+      events: {
+        'change input': 'onInputChange'
+      },
 
       initialize: function(settings) {
         this.settings = _.extend({}, this.defaults, settings);
@@ -46,7 +51,24 @@ define(
       },
 
       render: function() {
-        this.$el.html(this.template({ slides: this.slides }));
+        this.$el.html(
+          this.template({
+            slides: this.slides,
+            bgImage: this.slides[this.activeIndex].bg,
+            activeTitle: this.slides[this.activeIndex].title,
+            activeContent: this.slides[this.activeIndex].content
+          })
+        );
+      },
+
+      onInputChange: function(hey) {
+        var self = this;
+        this.activeIndex = parseInt(hey.currentTarget.value, 10) || 0;
+        this.slides = _.map(this.slides, function(slide, index) {
+          slide.isCurrent = self.activeIndex === index;
+          return slide;
+        });
+        this.render();
       }
     });
 
