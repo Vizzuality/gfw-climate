dae57618335c772503e67e651392a7d194e65262
j8seangel <jseangel@gmail.com>
2018-08-23 13:18:11 +0200

    Update store reservoirs values

--- app/assets/javascripts/insights/views/carbon-cycle/StoreReservoirView.js
+++ app/assets/javascripts/insights/views/carbon-cycle/StoreReservoirView.js
@@ -14,28 +14,43 @@ define(
       slides: [
         {
           id: 1,
-          title: 'Tropical peatland forests',
-          legend: '',
-          img: '/assets/insights/carbon-cycle/slider1@2x.jpg'
+          title: 'Tropical dry forest',
+          legend: '65 t C/ha',
+          img: '/assets/insights/carbon-cycle/tropical-dry@2x.jpg'
         },
         {
           id: 2,
-          title: 'Tropical primary forests',
-          legend: '',
-          img: '/assets/insights/carbon-cycle/slider2@2x.jpg'
+          title: 'Tropical Rainforest',
+          legend: '150 t C/ha',
+          img: '/assets/insights/carbon-cycle/tropical-rainforest@2x.jpg'
         },
         {
           id: 3,
-          title: 'Tropical secondary forests',
-          legend: '',
-          img: '/assets/insights/carbon-cycle/slider3@2x.jpg'
+          title: 'Temperate forest',
+          legend: '7 t C/ha',
+          sublegend:
+            'Average of oceanic (90 t C/ha), continental (60 t C/ha) and mountain forest (50 t C/ha)',
+          img: '/assets/insights/carbon-cycle/temperate-forest@2x.jpg'
         },
         {
           id: 4,
-          title:
-            'Tropical dry forests Temperate forests (broadleaf and temperate)',
-          legend: '',
-          img: '/assets/insights/carbon-cycle/slider4@2x.jpg'
+          title: 'Boreal forest',
+          legend: '20 t C/ha',
+          sublegend:
+            'Average of coniferous (25 t C/ha) and mountain forest (15 t C/ha) (edited)',
+          img: '/assets/insights/carbon-cycle/boreal-forest@2x.jpg'
+        },
+        {
+          id: 5,
+          title: 'Subtropical humid forest',
+          legend: '110 t C/ha',
+          img: '/assets/insights/carbon-cycle/subtropical-humid@2x.jpg'
+        },
+        {
+          id: 6,
+          title: 'Subtropical dry forest',
+          legend: '65 t C/ha',
+          img: '/assets/insights/carbon-cycle/subtropical-dry@2x.jpg'
         }
       ],
       template: Handlebars.compile(tpl),
