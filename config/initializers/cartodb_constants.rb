a06d4576de4acabb85c9b9d15e732893907b5d76
Simao Belchior <simaobelchior@gmail.com>
2018-09-14 16:32:20 +0100

    Update table name

--- config/initializers/cartodb_constants.rb
+++ config/initializers/cartodb_constants.rb
@@ -1,5 +1,5 @@
 ADMIN_BOUNDARY_ID = "admin"
-CDB_INDICATORS_VALUES_TABLE = "gfw_climate_country_pages_indicator_values_2017_data_20180828"
+CDB_INDICATORS_VALUES_TABLE = "gfw_climate_country_pages_indicator_values_2017_data_20180913"
 CDB_INDICATORS_TABLE = "gfw_climate_country_pages_indicator_info_2017_data"
 CDB_BOUNDARIES_TABLE="gfw_climate_country_pages_boundary_info_2017_data"
 CDB_COUNTRIES_TABLE = "gadm28_countries"
